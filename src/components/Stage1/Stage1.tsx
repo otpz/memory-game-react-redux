c734633f817d3b2de017377fbf9b26e62f499e08
Osman Topuz <osmantopuz98@gmail.com>
2024-09-19 00:45:28 +0300

    stage 1 completed

--- src/components/Stage1/Stage1.tsx
+++ src/components/Stage1/Stage1.tsx
@@ -3,14 +3,14 @@ import styles from "./style.module.css"
 
 const Stage1 = () => {
   const [gameHasStarted, setGameHasStarted] = useState<boolean>(true)
-
+  
   const [placesMatrixCopy] = useState<number[][]>([
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0]
+    [0, 0, 0, 1],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+    [0, 0, 0, 1]
   ])
-
+  
   const [clickedPlaces, setClickedPlaces] = useState<number[][]>([
     [0, 0, 0, 0],
     [0, 0, 0, 0],
@@ -18,33 +18,37 @@ const Stage1 = () => {
     [0, 0, 0, 0]
   ])
 
+  const [count, setCount] = useState<number>(clickedPlaces.length-1)
+  const [life, setLife] = useState<number>(2)
+
   useEffect(() => {
     // hide the right places 3 seconds after game start,
-    setTimeout(() => {
-      setGameHasStarted(false)
-    }, 1000)
-  }, [])
+    const timer = setTimeout(() => setGameHasStarted(false), 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleColClicked = (rowIdx: number, colIdx: number) => {
-    if (placesMatrixCopy[rowIdx][colIdx] === 1){
+
+    if (rowIdx !== count){
+      return
+    }
+
+    if (placesMatrixCopy[rowIdx][colIdx] === 1) {
       setClickedPlaces(prev => {
-        return prev.map((row, rowIdxMap) => {
-          if (rowIdxMap === rowIdx){
-            return row.map((col, colIdxMap) => {
-              if (colIdxMap === colIdx){
-                console.log(col, colIdxMap)
-                return 1
-              }
-              return col
-            })
-          }
-          return row
-        })
+        const updatedPlaces = prev.map(row => [...row])
+        updatedPlaces[rowIdx][colIdx] = 1
+        return updatedPlaces
       })
+      setCount(prev => prev - 1)
+    } else {
+      setClickedPlaces(prev => {
+        const updatedPlaces = prev.map(row => [...row])
+        updatedPlaces[rowIdx][colIdx] = 2
+        return updatedPlaces
+      })
+      setLife(prev => prev - 1)
     }
   }
-
-
 
   return (
     <div className={styles.container}>
@@ -61,15 +65,14 @@ const Stage1 = () => {
           ))
         })
       }
-
       {
         !gameHasStarted && clickedPlaces.map((row, rowIdx) => {
           return row.map((col, colIdx) => (
             <div onClick={() => handleColClicked(rowIdx, colIdx)} key={colIdx} className={styles.col}>
               {
-                col === 1 ?
-                <div className={styles.green_box}></div> :
-                null
+                col === 1 ? 
+                <div className={styles.green_box}></div> : 
+                (col === 2 ? <div className={styles.red_box}></div> : null)
               }
             </div>
           ))
